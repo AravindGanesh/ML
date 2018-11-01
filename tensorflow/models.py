4eb7788add0e34cb4c9d5d37c84f75337def175f
AravindGanesh <ee16btech11026@iith.ac.in>
2018-11-01 12:56:41 +0530

    Initialize self-assigned work

---
+++ tensorflow/models.py
@@ -0,0 +1,11 @@
+'''classes for all the models written 
+* Simple MLP (try for custom layers)
+* KNN
+* SVM
+* K-means
+* GMM
+* Autoencoder
+* Convolutional Autoencoder
+* Variational Autoencoder
+* Convolutional VAE
+'''
